ade10713d76f8fb8efa615171b62a5dd2f2915f8
Pashnik <Fktrcfy12>
2018-05-26 16:16:14 +0300

    splitting on tokens

--- lexer.c
+++ lexer.c
@@ -1,18 +1,53 @@
 #include "stdio.h"
+#include "ctype.h"
+#include "string.h"
 
-void getToken();
+int getToken(void);
+
+int isSomething(char symbol);
+
+int isGap(char symbol);
 
 enum tokenType {
-    DIGIT, OPERAND, VARIABLE, COMMAND, E_O_L
+    DIGIT, OPERATOR, VARIABLE, COMMAND, E_O_L
 };
 
 char *token_pointer; // глобальный указатель на текущий адрес символа
+char currentToken[50];
 
-void getToken() {
-
+int getToken(void) {
+    if (*token_pointer == '\n' || *token_pointer == '\0') return E_O_L; // провекра на конец строки
+    while (isGap(*token_pointer)) ++token_pointer;
+    if (isdigit(*token_pointer)) {
+        unsigned int i = 0;
+        while (!isSomething(*(token_pointer + 1))) {
+            currentToken[i] = *token_pointer;
+            i++;
+            ++token_pointer;
+        }
+        ++token_pointer;
+        return DIGIT;
+    }
+    if (strchr("+-*/=()", *token_pointer)) {
+        ++token_pointer;
+        return OPERATOR;
+    }
+    return 404; // ошибка
 }
 
 void execute(char *result_string) {
     token_pointer = result_string;
+    for (int i = 0; i < 8; ++i) {
+        printf("%d\n", getToken());
+    }
 }
 
+int isGap(char symbol) {  // пробел
+    if (symbol == ' ') return 1;
+    return 0;
+}
+
+int isSomething(char symbol) {
+    if (strchr("()=/*+- ", symbol)) return 1;
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include "interpreter.h"
 
-#define MAX_LINE 254
+#define MAX_LINE 200
 
 char *readFile(char *fileName);
 
-int main() {
-    char *name = "/Users/air/ClionProjects/BasicInterpreter/file.txt";
+int main(void) {
+    char *name = "/Users/air/ClionProjects/BasicIntepreter/newFile.txt";
     execute(readFile(name));
+    return 0;
 }
-
 
 char *readFile(char *fileName) {  /*Возможно динамическое выделение памяти для строки < 255 символа*/
     FILE *file;
-    char line[MAX_LINE];
+    char line[MAX_LINE] = {};
     char *line_pointer = line;
-    int symbol, i = 0;
+    int symbol;
+    unsigned int i = 0;
     file = fopen(fileName, "r");
     if (file == NULL) perror("Error in opening");
     else {
         do {
             symbol = fgetc(file);
-            line[i] = (char) symbol;
+            *(line_pointer + i) = (char) symbol;
             i++;
         } while (symbol != EOF);
     }
