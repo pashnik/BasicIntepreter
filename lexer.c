5f4b7c64f67b56707f08e68a6c93ba6a84ad2aac
Pashnik <Fktrcfy12>
2018-05-27 00:09:06 +0300

    more for getToken

--- lexer.c
+++ lexer.c
@@ -12,12 +12,15 @@ int isSomething(char symbol);
 
 int isGap(char symbol);
 
+int haveCommand(const char *input);
+
 enum tokenType {
     DIGIT, OPERATOR, VARIABLE, COMMAND, E_O_L
 };
 
+char *commands[] = {"input", "print", "let", "if", "goto"};
 char *token_pointer; // глобальный указатель на текущий адрес символа
-char currentToken[50];
+char currentToken[5];
 
 int getToken(void) {
     if (*token_pointer == '\n' || *token_pointer == '\0') return E_O_L; // провекра на конец строки
@@ -26,7 +29,7 @@ int getToken(void) {
         unsigned int i = 0;
         while (!isSomething(*(token_pointer + 1))) {
             currentToken[i] = *token_pointer;
-            i++;
+            ++i;
             ++token_pointer;
         }
         ++token_pointer;
@@ -39,14 +42,16 @@ int getToken(void) {
     if (isalpha(*token_pointer)) { // команда или переменная
         if (isalpha(*(token_pointer + 1))) { // команда
             unsigned int i = 0;
-            while (isalpha(*(token_pointer + 1))) {
+            while (isalpha(*(token_pointer))) {
                 currentToken[i] = *token_pointer;
-                i++;
+                ++i;
                 ++token_pointer;
             }
-            ++token_pointer;
-            return COMMAND;
-        } else {
+            if (haveCommand(currentToken)) {
+                ++token_pointer;
+                return COMMAND;
+            } else return 404;
+        } else { // переменная
             ++token_pointer;
             return VARIABLE;
         }
@@ -80,7 +85,19 @@ int isSomething(char symbol) {
 
 void interpret(char *line) {
     token_pointer = line;
-    for (int i = 0; i < 8; ++i) {
-        printf("%d", getToken());
+    for (int i = 0; i < 11; ++i) {
+        printf("%d\n", getToken());
     }
+}
+
+int haveCommand(const char *input) {
+    for (int j = 0; j < 5; ++j) {
+        unsigned int sum = 0;
+        char *current_command = commands[j];
+        for (int i = 0; i < strlen(current_command); ++i) {
+            if (input[i] == current_command[i]) sum++;
+        }
+        if (sum == strlen(current_command)) return 1;
+    }
+    return 0;
 }
